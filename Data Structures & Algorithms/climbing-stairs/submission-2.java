3176d05bf023bf9d82df1abbe3fd48a529d873c1
Shashank Agrawal <154488267+LordCenk@users.noreply.github.com>
2026-07-31 18:03:36 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        Integer[] memo = new Integer[n + 1];
+        return dfs(n, 0, memo);
+    }
+    public int dfs(int n, int i, Integer[] memo){
+        if(i >= n) return i == n ? 1 : 0;
+        if(memo[i] != null) return memo[i];
+        return memo[i] = dfs(n, i + 1, memo) + dfs(n, i + 2, memo);
+    }
+}
